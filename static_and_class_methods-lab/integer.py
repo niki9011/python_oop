e0ff4bc5e1ba82ea16de707a473f88b2915582a7
Nikolay Videnov <niki9011@abv.bg>
2023-03-09 23:00:12 +0200

    new task

---
+++ static_and_class_methods-lab/integer.py
@@ -0,0 +1,41 @@
+class Integer:
+
+    def __init__(self, value: int):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value: float):
+        if not isinstance(float_value, float):
+            return "value is not a float"
+
+        return cls(int(float_value))
+
+    @classmethod
+    def from_roman(cls, value: str):
+        rom_val = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        number = 0
+
+        for i in range(len(value)):
+            if i != 0 and rom_val[value[i]] > rom_val[value[i - 1]]:
+                number += rom_val[value[i]] - rom_val[value[i - 1]] - 1
+            else:
+                number += rom_val[value[i]]
+
+        return cls(number)
+
+    @classmethod
+    def from_string(cls, value: str):
+        if not isinstance(value, str):
+            return "wrong type"
+
+        return cls(int(value))
+
+
+first_num = Integer(10)
+print(first_num.value)
+
+second_num = Integer.from_roman("IX")
+print(second_num.value)
+
+print(Integer.from_float("2.6"))
+print(Integer.from_string(2.6))
